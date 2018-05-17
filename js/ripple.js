b1fb1f009c1c18d1731625671a730b099b411eca
bbm2h14abe <bbm2h14abe@bg.bib.de>
2018-05-17 14:24:36 +0200

    Finished button, put shadow and reset styles in their own files, added ripple.js.

---
+++ js/ripple.js
@@ -0,0 +1,22 @@
+var mouse_x = 0;
+var mouse_y = 0;
+
+document.onmousemove = ((event) => {
+    var dot, eventDoc, doc, body, pageX, pageY;
+    event = event || window.event; // IE-ism
+    mouse_x = event.pageX;
+    mouse_y = event.pageY;
+});
+
+function button_ripple(elem){
+    elem.classList.add("ripple");
+
+    var elem_rect = elem.getBoundingClientRect();
+    var local_x = mouse_x - elem_rect.left;
+    var local_y = mouse_y - elem_rect.top; 
+
+    var button = document.querySelector('.button>.background');
+    button.style.left = (local_x - 34) + "px";
+    button.style.top = (local_y - 34) + "px";
+    setTimeout(()=>{ elem.classList.remove("ripple"); }, 200);
+}
