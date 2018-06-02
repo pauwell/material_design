f0bdec8323f28e7a64afa73387dde1949f61571a
Paul Bernitz <paulbernitz@web.de>
2018-06-02 04:42:22 +0200

    Refactoring and a few ideas.

--- js/expandable.js
+++ js/expandable.js
@@ -1,5 +1,13 @@
 var initial_content_list = new Array();
 
+/* TODO: (use a simple list and search current elem in list each time instead of id)
+window.addEventListener("load", ()=>{
+    var expandables = document.querySelectorAll(".expandable");
+    expandables.forEach((elem, index, arr) => {
+        initial_content_list = elem.innerHTML;
+    });
+});*/
+
 function toggle_expandable(elem, id){
     if(initial_content_list[id] == null){
         initial_content_list[id] = elem.innerHTML;
